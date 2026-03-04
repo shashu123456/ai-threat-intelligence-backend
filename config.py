76b17aae773f571a2bd8ab7ca9be86e21f483515
unknown <kavitha@DESKTOP-E8JK13F.com>
2026-03-04 21:07:06 +0530

    Remove environment variable files properly

--- config.py
+++ config.py
@@ -2,8 +2,11 @@ import os
 from datetime import timedelta
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key")
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "jwt-secret-key")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "jwt-secret")
+
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
+
     DATABASE_URL = os.environ.get("DATABASE_URL")
+
     API_KEY = os.environ.get("API_KEY", "my-secure-api-key")
